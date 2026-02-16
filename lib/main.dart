ecb1cf1f4af2711c8011dc372f3459d42e6d2ec9
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-02-16 20:43:58 +0100

    Feat: Add combined list examples in step

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:rxdart/subjects.dart';
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() {
   runApp(const MyApp());
@@ -23,41 +28,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
+void testIt() async {
+  final stream1 = Stream.periodic(
+    const Duration(seconds: 1),
+    (count) => 'Stream 1, count= $count',
+  );
+  final stream2 = Stream.periodic(
+    const Duration(seconds: 3),
+    (count) => 'Stream 1, count= $count',
+  );
+  // combineLatest2 => mean combine 2 stream , combineLatest3 => mean combine 3 stream
+  final combined = Rx.combineLatest2(
+    stream1,
+    stream2,
+    (count1, count2) => 'one = ($count1) , two = ($count2)',
+  );
+
+  await for (final value in combined) {
+    value.log();
+  }
+}
+
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Every rebuild of this widget will create a new BehaviorSubject, but it will only be created once due to useMemoized => key ref to super.key.
-    final subject = useMemoized(() => BehaviorSubject<String>(), [key]);
-    // useEffect will run the provided function when the widget is disposed, ensuring that the subject is properly closed to prevent memory leaks.
-    useEffect(() => subject.close, [subject]);
+    testIt();
     return Scaffold(
-      appBar: AppBar(
-        title: StreamBuilder(
-          stream: subject.stream.distinct().debounceTime(
-            const Duration(seconds: 1),
-          ),
-          initialData: 'Please start typing....',
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-            if (snapshot.hasData) {
-              return Text(snapshot.requireData);
-            }
-            return const Text('Type something');
-          },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: TextField(
-          onChanged: (value) {
-            subject.sink.add(value);
-          },
-        ),
-      ),
+      appBar: AppBar(title: AppBar(title: Text('Home Page'))),
     );
   }
 }
