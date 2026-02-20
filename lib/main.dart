3e70f209c10ec97ad0a5084d6aeea5fffcbe2c6b
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-02-20 16:52:05 +0100

    Fix: remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:rxdart/subjects.dart';
 
 void main() {
   runApp(const MyApp());
