9f8706ef83ffde187f812a9615a4b71d5369d72b
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-02-27 17:58:54 +0100

    Fix: remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
-import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_rxdart/features/app/presentation/bloc/app_bloc.dart';
-import 'package:flutter_rxdart/features/auth/domain/auth_error.dart';
 import 'package:flutter_rxdart/features/home/presentation/page/home_page.dart';
-import 'package:flutter_rxdart/features/views/presentation/bloc/current_view.dart';
-import 'package:flutter_rxdart/features/auth/presentation/widgets/auth_error_dialog.dart';
 import 'package:flutter_rxdart/config/firebase_options.dart';
-import 'package:flutter_rxdart/core/loading/loading_screen.dart';
-import 'package:flutter_rxdart/features/contacts/presentation/pages/add_contact_page.dart';
-import 'package:flutter_rxdart/features/contacts/presentation/pages/contacts_page.dart';
-import 'package:flutter_rxdart/features/auth/presentation/pages/login_page.dart';
-import 'package:flutter_rxdart/features/auth/presentation/pages/register_page.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
